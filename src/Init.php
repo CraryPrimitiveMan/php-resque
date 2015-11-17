a3a4ce5e6d195079e9dad38871e3e24096028db8
harrysun <sunguangjun@126.com>
2015-11-17 21:24:59 +0800

    Add the Init class

---
+++ src/Init.php
@@ -0,0 +1,15 @@
+<?php
+namespace resque;
+
+require_once __DIR__ . '/../autoload.php';
+
+/**
+ * InitTrait, a init file for worker
+ * @author Harry Sun <sunguangjun@126.com>
+ * @copyright 2015 Harry Sun <sunguangjun@126.com>
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+class Init
+{
+    use InitTrait;
+}
